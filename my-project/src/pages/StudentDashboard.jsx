6de170e7eb5e7b7ebe04e59097499e88bb8549c3
AayushAdhikari30 <aayush.adhikari.9464@gmail.com>
2025-12-07 11:23:16 +0545

    UI

--- my-project/src/pages/StudentDashboard.jsx
+++ my-project/src/pages/StudentDashboard.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-
+import React, { Component } from 'react'
+import Studentsidebar from '../componets/dashboard/Adminsidebar'
 const StudentDashboard = () => {
   return (
     <div>
-      
+      <Studentsidebar/>
+
     </div>
   )
 }
