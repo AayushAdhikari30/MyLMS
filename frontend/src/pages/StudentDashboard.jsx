d14f6ee1eb3384922068937cfc738d0bed70a879
AayushAdhikari30 <aayush.adhikari.9464@gmail.com>
2025-12-07 15:19:36 +0545

    UI

---
+++ frontend/src/pages/StudentDashboard.jsx
@@ -0,0 +1,21 @@
+<<<<<<< HEAD
+import React from 'react'
+
+const StudentDashboard = () => {
+  return (
+    <div>
+      
+=======
+import React, { Component } from 'react'
+import Studentsidebar from '../componets/dashboard/Adminsidebar'
+const StudentDashboard = () => {
+  return (
+    <div>
+      <Studentsidebar/>
+
+>>>>>>> Aayush
+    </div>
+  )
+}
+
+export default StudentDashboard
