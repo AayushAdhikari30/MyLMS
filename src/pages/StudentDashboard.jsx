0d98bb1fca3e658ea73bbc53e4866cc2164fb610
AayushAdhikari30 <aayush.adhikari.9464@gmail.com>
2025-12-08 17:51:44 +0545

    pull

--- src/pages/StudentDashboard.jsx
+++ src/pages/StudentDashboard.jsx
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
-import React from 'react'
-
-const StudentDashboard = () => {
-  return (
-    <div>
-      
-=======
 import React, { Component } from 'react'
 import Studentsidebar from '../componets/dashboard/Adminsidebar'
 const StudentDashboard = () => {
   return (
     <div>
       <Studentsidebar/>
-
->>>>>>> Aayush
     </div>
   )
 }
