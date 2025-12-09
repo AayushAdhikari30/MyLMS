b9d61f1418ae611c2eb1c4fdc5712f7bced2acee
AayushAdhikari30 <aayush.adhikari.9464@gmail.com>
2025-12-10 00:26:52 +0545

    Error

--- frontend/src/componets/dashboard/Navbar.jsx
+++ frontend/src/componets/dashboard/Navbar.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
-import { useAuth } from '../../context/authcontext'
+import React ,{useEffect,useState}from 'react'
+import axios from 'axios'
+import { getUser } from '../../fetch'
+import { useNavigate } from 'react-router-dom'
+
 
 const Navbar = () => {
-  const { user, logout } = useAuth()
+  const navigate = useNavigate()
+  const handleclick=() => {
+navigate('/login')
+  }
+  const [data,setData] = useState([]);
+  useEffect(() => {
+    getUser().then(posts => setData(posts))
+
+  } ,[]) 
   
   return (
     <div className='flex items-center justify-between h-12 bg-teal-600 px-6 text-white shadow-md'>
-      <p className='text-lg font-semibold'>Welcome, {user?.name || 'Admin'}</p>
+      <p className='pl-69 text-lg font-semibold'>Welcome, {data.length > 0 ? data[0]?.name : 'Admin'}
+</p>
+
       <button 
-        onClick={logout}
+        onClick={handleclick}
         className='bg-teal-700 hover:bg-teal-800 px-4 py-2 rounded-md transition-colors duration-200'
+        
       >
         Logout
       </button>
